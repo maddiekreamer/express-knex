299500c23ce138d0ce31ffa9e4f2d4a2d641187d
Maddie Kreamer <maddiekreamer@gmail.com>
2018-09-11 13:37:17 -0600

    knex file added and edited

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/express_knex_warmup'
+  },
+
+  staging: {
+
+  },
+
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL'
+  }
+
+};
