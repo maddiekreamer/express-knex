c7ceaa7ec65c7f89a64b1b568ed1101aea5aa77d
Maddie Kreamer <maddiekreamer@gmail.com>
2018-09-11 09:54:24 -0600

    route written

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on ${port}`))
 
+app.get("/", (request, response) => {
+    response.send('THE ROUTE WORKED!');
+});
