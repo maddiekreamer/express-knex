cf75a320ecc37ba4511f9c53edfba01ea08ffe66
Maddie Kreamer <maddiekreamer@gmail.com>
2018-09-13 13:37:38 -0600

    Cors required in app.js

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const queries = require('./queries');
+const cors = require("cors")
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
+
 app.get("/", (request, response, next) => {
-    queries.listAll().then(result => response.json({result}))
+    queries.listAll().then(result => response.json({
+        result
+    }))
 });
 
 app.get("/:id", (request, response, next) => {
-    queries.individualStudent(request.params.id).then(result => response.json({result}))
+    queries.individualStudent(request.params.id).then(result => response.json({
+        result
+    }))
 })
 
 app.post("/", (request, response, next) => {
-    queries.create(request.body).then(result => response.json({result}))
+    queries.create(request.body).then(result => response.json({
+        result
+    }))
 })
 
 app.delete("/:id", (request, response, next) => {
-    queries.deleteStudent(request.params.id).then(result => response.json({result}))
+    queries.deleteStudent(request.params.id).then(result => response.json({
+        result
+    }))
 })
 
 app.listen(port, () => {
